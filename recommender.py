41c34fcf8b8ce3b30bae1bf68cba5bcf9cf98f16
Joshua Stahl <neobaud@gmail.com>
2021-04-18 01:17:04 +0000

    Added database

--- recommender.py
+++ recommender.py
@@ -1,14 +1,34 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="neobaud",
+    password="600*$53IfrH!",
+    hostname="neobaud.mysql.pythonanywhere-services.com",
+    databasename="neobaud$recommender",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+comments = []
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments = comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
 
 @app.route('/wibble')
 def wibble():
