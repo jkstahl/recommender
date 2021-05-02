54b741e046c180a07dedaa27ff673b5b33448237
jkstahl <neobaud@gmail.com>
2021-05-01 19:57:18 -0500

    Made it always add path for new thumbs

--- image_to_thumb.py
+++ image_to_thumb.py
@@ -10,11 +10,14 @@ for image_fname in glob.glob('raw_images/*png')+ glob.glob('raw_images/*jpg') + 
     im2 = image.resize(thumb_size, Image.ANTIALIAS)
     thumb_path = os.path.join(thumb_dir, os.path.basename(image_fname))
     print (thumb_path)
-    base_name = os.path.basename(image_fname).split('.')[0].replace('_', ' ')
-    print (base_name)
+
+    im2.save(thumb_path)
+    
+# go back through and update all items with image path that are in static/thumbs already
+for thumb_path in glob.glob(os.path.join(thumb_dir, '*') ):
+    base_name = os.path.basename(thumb_path).split('.')[0].replace('_', ' ')
     item = r.Item.query.filter(r.func.lower(r.Item.name)==r.func.lower(base_name)).first() 
     if item != None:
         print ('Found image: %s' % base_name)
         item.image = thumb_path
         r.db.session.commit()
-    im2.save(thumb_path)
