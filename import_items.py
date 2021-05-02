34ce38edc1b3bf27502982f785b1f25e18cb3885
Joshua Stahl <neobaud@gmail.com>
2021-05-02 00:42:23 +0000

    fixed item import script

--- import_items.py
+++ import_items.py
@@ -1,14 +1,24 @@
 import recommender as r
-import sys
+import sys, glob, os
 
-
-with open('items/cars.csv',encoding='latin1') as fp:
-    first_line = fp.readline()
-    for line in fp:
-        cols = line.split(';')
-        make, model, _, _ = cols
-        name = make + ' ' + model
-        exists = r.db.session.query(r.Item.name).filter_by(name=name).first() is not None
-        if not exists:
-            r.db.session.add(r.Item(name = make + ' ' + model, category = 'Cars'))
+item_csvs = glob.glob('items/*csv')
+for csv in item_csvs:  # each csv is a category    
+    with open(csv, encoding='ascii', errors='ignore') as fp:
+        header = fp.readline().split(';')
+        category = os.path.basename(csv).split('.')[0].replace('_', ' ').capitalize()
+        for line in fp:
+            cols = line.strip().split(';')
+            names = []
+            for col, cn in enumerate(header):
+                if 'name' in cn:
+                    names.append( cols[col])
+            #print (str(names))
+            name = ' '.join(names)
+            #print (name)
+            if name == '': continue
+            exists = r.db.session.query(r.Item.name).filter_by(name=name).first() is not None
+            if not exists:
+                print ('found ' + name + ' ' + category)
+                r.db.session.add(r.Item(name = name, category = category))
+                
 r.db.session.commit()
